3fe78d67abced2e3d6e8c9b984490e23e25606c2
Andreykash <34689947+Andreykash@users.noreply.github.com>
2024-10-04 14:45:33 +0300

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
     "io"
     "math/rand"
     "net/http"
+    "os"
     "os/exec"
     "path/filepath"
     "time"
@@ -13,13 +14,64 @@ import (
 var musicDir = "/path/to/mounted/cloud/disk"
 
 func main() {
+    http.HandleFunc("/", dirHandler)
+    http.HandleFunc("/browse", browseHandler)
     http.HandleFunc("/stream", streamHandler)
     fmt.Println("Starting server on :8080")
     http.ListenAndServe(":8080", nil)
 }
 
+func dirHandler(w http.ResponseWriter, r *http.Request) {
+    dirs, err := os.ReadDir(musicDir)
+    if err != nil {
+        http.Error(w, "Unable to read directories", http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "text/html")
+    fmt.Fprintln(w, "<html><body><h1>Select Directory</h1><ul>")
+    for _, dir := range dirs {
+        if dir.IsDir() {
+            fmt.Fprintf(w, "<li><a href=\"/browse?dir=%s\">%s</a></li>", dir.Name(), dir.Name())
+        }
+    }
+    fmt.Fprintln(w, "</ul></body></html>")
+}
+
+func browseHandler(w http.ResponseWriter, r *http.Request) {
+    dir := r.URL.Query().Get("dir")
+    if dir == "" {
+        http.Error(w, "No directory selected", http.StatusBadRequest)
+        return
+    }
+
+    fullPath := filepath.Join(musicDir, dir)
+    items, err := os.ReadDir(fullPath)
+    if err != nil {
+        http.Error(w, "Unable to read directory", http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "text/html")
+    fmt.Fprintln(w, "<html><body><h1>Browse Directory</h1><ul>")
+    for _, item := range items {
+        if item.IsDir() {
+            fmt.Fprintf(w, "<li><a href=\"/browse?dir=%s\">%s</a></li>", filepath.Join(dir, item.Name()), item.Name())
+        } else {
+            fmt.Fprintf(w, "<li>%s</li>", item.Name())
+        }
+    }
+    fmt.Fprintf(w, "</ul><a href=\"/stream?dir=%s\">Stream from this directory</a></body></html>", dir)
+}
+
 func streamHandler(w http.ResponseWriter, r *http.Request) {
-    files, err := filepath.Glob(filepath.Join(musicDir, "*.mp3"))
+    dir := r.URL.Query().Get("dir")
+    if dir == "" {
+        http.Error(w, "No directory selected", http.StatusBadRequest)
+        return
+    }
+
+    files, err := filepath.Glob(filepath.Join(musicDir, dir, "*.mp3"))
     if err != nil || len(files) == 0 {
         http.Error(w, "No music files found", http.StatusInternalServerError)
         return
